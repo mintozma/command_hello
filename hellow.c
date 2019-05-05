32895021ce97a83144dd03aa80a1c428bbadc113
mintozma <50293612+mintozma@users.noreply.github.com>
2019-05-05 22:16:25 +0900

    hellow.c modified on GitHub

--- hellow.c
+++ hellow.c
@@ -2,6 +2,9 @@
 
 int main()
 {
+    //For command line git tutorial 
+    //GitHub modification
+    
     printf("hellow git\n");
     printf("tell your world\n");
     printf("tell his world\n");

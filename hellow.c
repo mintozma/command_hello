2a961079d4d29301cd1cc1d0925575f0fc80b2f7
Chang Hun Sung <emdtls110@gmail.com>
2019-05-05 23:12:22 -0400

    added output "--amend"

--- hellow.c
+++ hellow.c
@@ -5,7 +5,8 @@ int main()
     //local pc modification
     //For command line git tutorial 
     //GitHub modification
-    
+    // git commit --amend
+
     printf("hellow git\n");
     printf("tell your world\n");
     printf("tell his world\n");
